dac1d7cf752f62f335082fe2e39bca17a4bc1348
vanhongphuc <97586650+vanhongphuc@users.noreply.github.com>
2022-04-13 21:13:30 +0700

    Add files via upload

---
+++ baithuchanh1304/baithuchanh1304/src/App67.java
@@ -0,0 +1,8 @@
+public class App67 {
+  public static void main(String[] args){
+    HinhTron hinhTron = new HinhTron();
+    HinhTru hinhTru = new HinhTru();
+    hinhTron.xuatThongTin();
+    hinhTru.xuatThongTin();
+  }
+}
