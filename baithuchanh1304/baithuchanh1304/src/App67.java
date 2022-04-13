b45416940e8f6c773c499d5e069440d9bd6ca661
vanhongphuc <97586650+vanhongphuc@users.noreply.github.com>
2022-04-13 21:15:17 +0700

    Delete baithuchanh1304/baithuchanh1304 directory

--- baithuchanh1304/baithuchanh1304/src/App67.java
+++
@@ -1,8 +0,0 @@
-public class App67 {
-  public static void main(String[] args){
-    HinhTron hinhTron = new HinhTron();
-    HinhTru hinhTru = new HinhTru();
-    hinhTron.xuatThongTin();
-    hinhTru.xuatThongTin();
-  }
-}
